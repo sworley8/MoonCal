bd2f7db94d29cc785faf7c9b87ab34d9e8b5e8e1
Mysterious Siren <tanyaalana@gmail.com>
2017-01-21 19:11:03 -0500

    sugar again 3

--- app/src/main/java/com/example/sophi/myapplication/MainActivity.java
+++ app/src/main/java/com/example/sophi/myapplication/MainActivity.java
@@ -18,14 +18,42 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
+import java.util.Map;
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
+    Map <String,String[]> Pictures = new HashMap<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button json_button = (Button) findViewById(R.id.json_button);
+
+        Pictures.put("January", new String[] {"drawable/suga2","drawable/suga_bts"});
+        Pictures.put("February",new String[] {"drawable/bambam2","drawable,bambam"});
+        Pictures.put("March", new String[] {"drawable/v2","drawable/v"});
+        Pictures.put("April", new String[] {"drawable/jb2", "drawable/jb"});
+        Pictures.put("May", new String[] {"drawable/jimin", "drawable/jimin2"});
+        Pictures.put("June", new String[] {"drawable/kris2", "drawable/kris"});
+        Pictures.put("July", new String[] {"drawable/namjoon2", "drawable/namjoon"});
+        Pictures.put("August", new String[] {"drawable/jhope", "drawable/jhope2" });
+        Pictures.put("September", new String[] {"drawable/jin2", "drawable/jin"});
+        Pictures.put("October", new String[] {"drawable/kookie", "drawable/kookie2"});
+        Pictures.put("November", new String[] {"drawable/yug2", "drawable/yug"});
+        Pictures.put("December", new String[] {"drawable/mark", "drawable/mark2"});
+
+
+
+
+
+        Calendar cal=Calendar.getInstance();
+        SimpleDateFormat month_date = new SimpleDateFormat("MMMM");
+        String month_name = month_date.format(cal.getTime());
+
 
         json_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,20 +72,15 @@ public class MainActivity extends AppCompatActivity {
     class RetrieveFeedTask extends AsyncTask<Void, Void, String> {
 
         private Exception exception;
-        Calendar rightNow = Calendar.getInstance();
-        int month = rightNow.get(Calendar.MONTH);
-        int day = rightNow.get(Calendar.DAY_OF_MONTH);
-        int year = rightNow.get(Calendar.YEAR);
-
         private String APIurl =
-                "http://api.usno.navy.mil/moon/phase?date=" + Integer.toString(month) + "/" + Integer.toString(day) + "/" + Integer.toString(year) + "&nump=48";
+                "http://api.usno.navy.mil/moon/phase?date=5/3/2009&nump=48";
 
 
         protected String doInBackground(Void... urls) {
             // Do some validation here
 
             try {
-                java.net.URL url = new URL(APIurl);
+                java.net.URL url = new URL(APIurl /*"date"  "&nump=" + "&apiKey=" + API_KEY*/);
                 HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
                 try {
                     BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
@@ -86,7 +109,7 @@ public class MainActivity extends AppCompatActivity {
                 response = "THERE WAS AN ERROR";
             }
 
-            /*Log.i("INFO", response);*/
+            Log.i("INFO", response);
             try {
                 JSONObject jObject = new JSONObject(response);
                 JSONArray jArray = jObject.getJSONArray("phasedata");
@@ -96,7 +119,9 @@ public class MainActivity extends AppCompatActivity {
                     String date = oneObject.getString("date");
                     String time = oneObject.getString("time");
 
-
+                    Log.i("phase",phase);
+                        /*String jdate = jArray.get(i).getString(1);
+                         String jtime = jArray.get(i).getString(2);*/
                     }
 
                 Log.i("json", jArray.toString());
